c041369a2b9e6f13cca990f0db25d2be6b5601fc
Aniket Vyavahare <aniketvyavahare89301@gmail.com>
2026-01-06 23:32:26 +0530

    Nullish Coalescing OPerator

---
+++ Datastructures/Nullish_Caolescing.js
@@ -0,0 +1,61 @@
+"use strict";
+const restuarant = {
+  name: "Classic Italiano",
+  location: "Via Angelo Tavanti 23 ,Firenze Italy ",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruscheta", "Garlic Bread", "Caprese Salid"],
+  mainMenu: ["Pizza", "Pasta", "Risotto", "Lasagnia"],
+  // This Keyword to put
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order Recieved ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is a delicous pasta with 
+    ${ing1},${ing2} and ${ing3}`);
+  },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+restuarant.numguests = 0;
+const guests = restuarant.numguests || 10;
+console.log(guests);
+// For the or operator the 0 is a Falsy Value  .
+// Nullish Coalescing OPerator
+const guestsCorrect = restuarant.numguests ?? 10;
+console.log(guestsCorrect);
+
+/**
+ * Nullish Coalescing operator works with
+ * the nullish values .
+ *
+ * Nullish : null ,Undefined (Not 0 or ' ' );
+ *
+ */
